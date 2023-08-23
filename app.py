4f96048861c7acc9bfe59b3ab3063e3e9abb97f2
SpiceySauce <55515534+SpiceySauce@users.noreply.github.com>
2023-08-22 22:17:19 -0500

    Add widgets

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import gspread
-import tkinter
+import tkinter as tk
 from google.auth import exceptions
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-top = tkinter.Tk()
+frm = tk.Tk()
+frm.geometry('400x200')
 
 SCOPES = [
     'https://www.googleapis.com/auth/spreadsheets',
@@ -34,9 +35,19 @@ def main():
     spreadsheet = client.open('test-sheet')
     worksheet = spreadsheet.get_worksheet(0)  # 0 represents the first sheet
 
-    new_row = ["Data3", "Data4", "Data5"]
+    new_row = ["Data5", "Data6", "Data7"]
     worksheet.append_row(new_row)
 
     print("New row added successfully!")
     
-top.mainloop()
+
+B = tk.Button(frm, text ="Add to Spreadsheet", command = main)
+course_name = tk.Text(frm, height = 1,width = 10)
+hw_details = tk.Text(frm, height = 1,width = 10)
+due_date = tk.Text(frm, height = 1,width = 10)
+
+B.pack(padx=10, pady=10, side='bottom')
+course_name.pack(padx=10, pady=10, side='left')
+hw_details.pack(padx=10, pady=10, side='left')
+due_date.pack(padx=10, pady=10, side='left')
+frm.mainloop()
